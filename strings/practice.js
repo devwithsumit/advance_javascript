6064381f0006be0a46e4ead165f44c4b4eaff81a
webwithsumit <150454643+webwithsumit@users.noreply.github.com>
2024-06-19 00:33:52 +0530

    .

---
+++ strings/practice.js
@@ -0,0 +1,23 @@
+// console.log("sum\"".length);
+
+//1. startwith and endWith function of strings
+// let s1 = 'sumit'
+// console.log(s1.startsWith('s'));
+// console.log(s1.endsWith('t'));
+// console.log(s1.padEnd(7,'x'));// minimum 7 characters add 's' at end i less
+// console.log(s1.padStart(7, 'x'));//minimum 7 characters add 's' at start i less
+
+//2. extract 1000 from below string
+// let s2 = 'please give me ₹ 1000'
+// search = '1000';
+// console.log(s2.indexOf(search));
+// index = s2.indexOf(search)
+// // const numbers = s2.match(/\d+/g)
+// // console.log(numbers);
+// console.log(s2.substring(index));
+
+// 3. try to change 4th character of a given string !
+//Ans ; - no , can't change directly
+let s1  = 'given string'
+s1[4] = 'd'
+console.log(s1[4]);
