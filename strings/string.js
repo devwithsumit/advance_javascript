be779be5654c82ee13b908a82c489f94e5296c03
webwithsumit <150454643+webwithsumit@users.noreply.github.com>
2024-06-18 23:49:06 +0530

    string methods

---
+++ strings/string.js
@@ -0,0 +1,13 @@
+let s1 = ' String\'s demo '
+let s2 = 'sample'
+//string properties and methods
+// console.log(s1.length);
+// console.log(s1.toLowerCase());
+// console.log(s1.toUpperCase());
+// console.log(s1.slice(0,6))
+// console.log(s1.slice(4)) // 4 to end
+console.log(s1.replace("S", 's'))
+console.log(s1.replace("demo", 'tutortial'))
+console.log(s1.concat(s2,' is concated'));
+console.log(s1+s2); // or method
+console.log(s1.trim());
