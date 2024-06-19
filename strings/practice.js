3a7a7c57563c50549984f09dedf5284add4ce2a2
webwithsumit <150454643+webwithsumit@users.noreply.github.com>
2024-06-19 17:39:30 +0530

    strings

--- strings/practice.js
+++ strings/practice.js
@@ -8,16 +8,23 @@
 // console.log(s1.padStart(7, 'x'));//minimum 7 characters add 's' at start i less
 
 //2. extract 1000 from below string
-// let s2 = 'please give me ₹ 1000'
-// search = '1000';
-// console.log(s2.indexOf(search));
-// index = s2.indexOf(search)
-// // const numbers = s2.match(/\d+/g)
-// // console.log(numbers);
-// console.log(s2.substring(index));
+let s2 = 'please give me ₹ 1000'
+search = '1000';
+index = s2.indexOf(search)
+console.log(s2.slice(index));
+//or
+console.log(s2.substring(17));
+// or 
+money = s2.slice('please give me ₹ '.length)
+console.log(money);
 
 // 3. try to change 4th character of a given string !
 //Ans ; - no , can't change directly
-let s1  = 'given string'
-s1[4] = 'd'
-console.log(s1[4]);
+// let s1  = 'given string'
+// s1[4] = 'd'
+// console.log(s1[4]);
+
+// 4.ternary operator
+// const sent = 'the quick brown fox jumps over the lazy dog'
+// const find = 'dog';
+// console.log(`the ${find} ${sent.includes(find) ? 'is':'is not'} present`);
