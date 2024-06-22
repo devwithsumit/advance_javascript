c7e90d5155e1e8cc6ecedcf71e09662a0853748d
webwithsumit <sumit2410kushwaha@gmail.com>
2024-06-22 20:30:05 +0530

    alert and confirm methods

---
+++ JS_inBrowser/alertConfirm.js
@@ -0,0 +1,10 @@
+//alert()
+alert('hey what\'s up');
+
+//prompt("Message","default value opational")
+let a = prompt('write text to a page','hello everyone')
+
+//confirm()
+write = confirm('do you want to write it to the page')
+if (write) document.write(a)
+else console.log(write);
