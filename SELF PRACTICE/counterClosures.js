7f9a375604f831c021b6494ba4bdb4ff3ef898d3
webwithsumit <sumit2410kushwaha@gmail.com>
2024-09-04 23:37:14 +0530

    counter using closures

---
+++ SELF PRACTICE/counterClosures.js
@@ -0,0 +1,11 @@
+function createCounter() {
+    let count = 0;
+    return function () {
+        count += 1;
+        return count;
+    };
+}
+
+const counter = createCounter();
+console.log(counter()); // Output: 1
+console.log(counter()); // Output: 2
