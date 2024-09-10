fe07ab9d8f328ed98faf974792482bf2ff57ffb6
webwithsumit <sumit2410kushwaha@gmail.com>
2024-09-11 00:12:29 +0530

    Merge Two Sorted Arrays

---
+++ SELF PRACTICE/mergingTwoArrays.js
@@ -0,0 +1,19 @@
+function mergeSortedArrays(arr1, arr2) {
+    let mergedArray = [];
+    let i = 0, j = 0;
+
+    while (i < arr1.length && j < arr2.length) {
+        if (arr1[i] < arr2[j]) {
+            mergedArray.push(arr1[i]);
+            i++;
+        } else {
+            mergedArray.push(arr2[j]);
+            j++;
+        }
+    }
+
+    // Append the remaining elements
+    return mergedArray.concat(arr1.slice(i)).concat(arr2.slice(j));
+}
+
+console.log(mergeSortedArrays([1, 3, 5], [2, 4, 6])); // Output: [1, 2, 3, 4, 5, 6]
